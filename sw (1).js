3c949a615540dd50017c75c178aff338bdf25f18
peraltaoscar578-hue <peraltaoscar578@gmail.com>
2026-04-26 12:18:16 -0400

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,33 @@
+const CACHE = 'arcafe-v3';
+const FILES = ['./index.html', './manifest.json', './logo.png', './icon-192.png', './icon-512.png'];
+
+// Install: cache all files fresh
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+// Activate: delete ALL old caches immediately
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: network first, fallback to cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
